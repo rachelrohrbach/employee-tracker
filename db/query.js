082f6f9dc834d4455b57d55d5d88d62007a333f3
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-02-21 23:59:11 -0800

    created DB class with constructor functions for importing as asynchronous functions into server.js file

---
+++ db/query.js
@@ -0,0 +1,89 @@
+const connection = require("./connection");
+
+class DB {
+  constructor(connection) {
+    this.connection = connection;
+  }
+
+  viewAllEmployees() {
+    return this.connection.query(
+      `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+             FROM employee
+             LEFT JOIN role 
+             ON employee.role_id = role.id
+             LEFT JOIN department 
+             ON role.department_id = department.id 
+             LEFT JOIN employee manager 
+             ON manager.id = employee.manager_id;
+             `
+    );
+  }
+
+  viewAllRoles() {
+    return this.connection.query(
+      `SELECT role.id, role.title, role.salary, department.name AS department
+        FROM role 
+        LEFT JOIN department
+        ON role.department_id = department.id;`
+    );
+  }
+
+  viewAllDepartments() {
+    return this.connection.query("SELECT * from department");
+  }
+
+  addEmployee(first_name, last_name, role_id, manager_id) {
+    return this.connection.query("INSERT INTO employee SET ?", {
+      first_name: first_name,
+      last_name: last_name,
+      role_id: role_id,
+      manager_id: manager_id
+    });
+  }
+
+  addRole(role, salary) {
+    return this.connection.query("INSERT INTO role SET ?", {
+      title: role,
+      salary: salary
+    });
+  }
+
+  addDepartment(department) {
+    return this.connection.query("INSERT INTO department SET ?", {
+      name: department
+    });
+  }
+
+  findEmployee() {
+    return this.connection.query(
+      "SELECT id, first_name, last_name FROM employee"
+    );
+  }
+
+  findRoles() {
+    return this.connection.query("SELECT id, title FROM role");
+  }
+
+  updateEmployeeRole(role_id, id) {
+    return this.connection.query(
+      `UPDATE employee
+        SET role_id = ? 
+        WHERE id = ?;`,
+      [role_id, id]
+    );
+  }
+
+  // deleteEmployee() {
+  //   return this.connection.query(
+  //     "DELETE FROM employee SET ?", {
+  //       id: id
+  //     }
+  //   );
+  // }
+
+  exit() {
+    this.connection.end();
+  }
+}
+
+module.exports = new DB(connection);
